c8443d94d8575f18c10192230e9fb2d292efdc96
AhmadMujtaba200210 <cadeta6@gmail.com>
2026-02-18 09:02:04 -0500

    chore: applied mono repo structure

---
+++ packages/types/cms.ts
@@ -0,0 +1,8 @@
+export interface BlogPost {
+    id: string;
+    title: string;
+    slug: string;
+    content: string;
+    published: boolean;
+    publishedAt?: Date;
+}
---
+++ packages/types/portfolio.ts
@@ -0,0 +1,13 @@
+export interface Project {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    imageUrl?: string;
+}
+
+export interface Skill {
+    name: string;
+    icon: string;
+    color: string;
+}
